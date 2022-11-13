8f484d22cdbb43272ba1e233aa735a812d7bb6c5
Niran Algrably <niranalgrably@Nirans-MacBook-Air.local>
2022-11-13 17:57:35 +0200

    ex

--- localStorage ex/main.js
+++ localStorage ex/main.js
@@ -3,8 +3,6 @@ const email = document.querySelector(".email");
 const password = document.querySelector(".password");
 const btnLogin = document.querySelector(".btn-login");
 
-// userName.innerHTML = `${users[0].name}`;
-
 function uniqueToken() {
   const timeStamp = new Date().getTime().toString(36);
   const tokenUpCase = `${Math.random().toString(36).toUpperCase().substr(2)}`;
@@ -15,109 +13,76 @@ function uniqueToken() {
   const token = tokeLowwerCase + tokenUpCase + timeStamp;
   return token;
 }
-uniqueToken();
 
-let userObject = `{"name":"","email":"","token":""}`;
+let users = [
+  { name: "Niro", email: "niranal66@gmail.com", token: "", password: "1234" },
+  { name: "Avi", email: "aviv@123", token: "", password: "1234" },
+  { name: "Dan", email: "dan@1", token: "", password: "" },
+  { name: "Meir", email: "meir@2", token: "", password: "" },
+  { name: "Rina", email: "rina@3", token: "", password: "" },
+];
 
-let user = {};
-let usersJsonC = localStorage.user;
-if (usersJsonC) {
-  user = JSON.parse(userObject);
-} else {
-  localStorage.user = userObject;
-}
-
-let usersJson = `[{"name":"Niro","email":"niranal66@gmail.com","token":"","password":"1234"},{"name":"Avi","email":"aviv@123","token":"","password":"1234"},{"name":"Dan","email":"dan@1","token":"","password":""},{"name":"Meir","email":"meir@2","token":"","password":""},{"name":"Rina","email":"rina@3","token":"","password":""}]`;
-
-let users = {};
 let usersJsonIn = localStorage.usersInfo;
 
 if (usersJsonIn) {
   users = JSON.parse(usersJsonIn);
 } else {
-  localStorage.usersInfo = usersJson;
+  localStorage.usersInfo = JSON.stringify(users);
+  loginEmail();
 }
 
-for (let i = 0; i < users.length; i++) {
-  user.token = users[i].token;
-  const userI = users[i];
-  console.log(userI);
+let userObject = null;
 
-  function tokenLogin() {
-    if (user.token == users[i].token) {
-      alert("hh");
-      users[i].token = uniqueToken();
-      user.token = users[i].token;
-      user.name = users[i].name;
-      user.email = users[i].email;
-      localStorage.usersInfo = JSON.stringify(users);
-      localStorage.user = JSON.stringify(user);
-    } else return;
-  }
+let user = {};
+let userJsonC = localStorage.user;
+if (userJsonC) {
+  user = JSON.parse(userJsonC);
+  tokenLogin();
+} else {
+  loginEmail();
+}
 
-  console.log(!users[i].token);
-
-  function loginEmail() {
-    if (!user.name || !users[i].token) {
-      console.log("login please");
-      btnLogin.addEventListener("click", function () {
-        console.log(
-          email.value == users[0].email && password.value == users[0].password,
-          email.value == users[1].email && password.value == users[1].password,
-          email.value == users[2].email && password.value == users[2].password,
-          email.value == users[3].email && password.value == users[3].password,
-          email.value == users[4].email && password.value == users[4].password
-        );
-        if (
-          email.value == users[0].email &&
-          password.value == users[0].password
-        ) {
-          users[0].token = uniqueToken();
-          user.name = users[0].name;
-          user.email = users[0].email;
-          user.token = users[0].token;
-          localStorage.user = JSON.stringify(user);
-          console.log("login in");
-          console.log(users[0].name);
-        }
+function tokenLogin() {
+  for (let i = 0; i < users.length; i++) {
+    const u = users[i];
+    if (u.email == user.email) {
+      if (u.token == user.token) {
+        user.token = uniqueToken(); // new token
+        u.token = user.token; // meadken
+        localStorage.user = JSON.stringify(user); // meadken LS
+        localStorage.usersInfo = JSON.stringify(users); // meadken LS
         welcomwUser();
-      });
+
+        return;
+      }
     }
   }
+  loginEmail();
 }
 
-function stayCheckIn() {
-  if (user.name == users[0].name && user.token == users[0].token) {
-    user.name = users[0].name;
-    user.email = users[0].token;
-    localStorage.user = JSON.stringify(user);
-    console.log("stay!!!!!");
-  }
+function loginEmail() {
+  console.log("login please");
+  btnLogin.addEventListener("click", function () {
+    for (let i = 0; i < users.length; i++) {
+      const u = users[i];
+      if (email.value == u.email) {
+        if (password.value == u.password) {
+          user.token = uniqueToken(); // new token
+          user.email = u.email;
+          user.name = u.name;
+          u.token = user.token; // meadken
+          localStorage.user = JSON.stringify(user); // meadken LS
+          localStorage.usersInfo = JSON.stringify(users); // meadken LS
+          welcomwUser();
+          found = true;
+          return;
+        }
+      }
+    }
+    alert("wrong info");
+  });
 }
-loginEmail();
-stayCheckIn();
-tokenLogin();
 
 function welcomwUser() {
   userName.innerHTML = `Welcome ${user.name}`;
-  users = JSON.parse(usersJsonIn);
 }
-
-// console.log(users[i].token);
-
-// if (confirm("Do you want to change your password?")) {
-//   localStorage.password = prompt("enter a new password");
-// }
-// users.email = "niranal66@gamil.com";
-// users.password = 123456;
-
-// if (users.password == password && users.email == email) {
-//   alert("welcomw");
-// } else alert("wrong info");
-
-// try {
-//   users = JSON.parse(usersJson);
-//   console.log(users);
-// } catch (e) {}
-
-// users.kids.push({ fName: "Rani", age: 2 });
